2577ef827f019941afa5d6ef53aeef5ba6d0b57c
Tony Morris <afmorris@outlook.com>
2016-02-01 18:31:47 -0500

    Initial check-in. This should be in. :bomb:

---
+++ AutofacIocAdapter/AutofacIocAdapter.cs
@@ -0,0 +1,32 @@
+﻿namespace AutofacIocAdapter
+{
+    using Autofac;
+    using ServiceStack.Configuration;
+
+    public class AutofacIocAdapter : IContainerAdapter
+    {
+        private readonly IContainer container;
+
+        public AutofacIocAdapter(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public T Resolve<T>()
+        {
+            return this.container.Resolve<T>();
+        }
+
+        public T TryResolve<T>()
+        {
+            T result;
+
+            if (this.container.TryResolve(out result))
+            {
+                return result;
+            }
+
+            return default(T);
+        }
+    }
+}
